60df6611ecb4389b86bb502d407fa324b47b4d1a
Ratul Maharaj <ratulmaharaj@gmail.com>
2023-10-04 01:51:20 +0200

    Add starter app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn get_message() -> String {
+    // create a list of messages
+    const MESSAGES: [&str; 5] = [
+        "Rustic Greetings! 🦀✨",
+        "Rust: Fearless Concurrency in Action! 🚀",
+        "Climbing the Learning Curve in Rust! 🧗‍♂️",
+        "Rust: Where Ideas Compile! ✨💻",
+        "Unlocking the Power of Rust! 🔓🦀",
+    ];
+
+    // generate a random number between 0 and 4
+    let random_number = rand::random::<usize>() % 4;
+
+    // return a random message
+    MESSAGES[random_number].to_string()
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_message])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
